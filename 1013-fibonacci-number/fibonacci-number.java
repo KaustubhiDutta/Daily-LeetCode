19f3ba033ca2fc0eb649f27bd52dc74ad7a46828
Kaustubhi Dutta <kaustubhidutta2006@gmail.com>
2026-08-04 02:59:58 +0530

    Time: 10 ms (26.12%) | Memory: 42 MB (70.96%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+//using recursion
+class Solution {
+    public int fib(int n) {
+
+        int sum = 0;
+        if( n == 0 || n== 1){
+            return n ;
+        }
+
+        sum = fib(n-1)+fib(n-2);
+        return sum;
+    }
+}
