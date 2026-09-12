4dc40af89f137b387e0b748918af99eb20bc21af
Kaustubhi Dutta <kaustubhidutta2006@gmail.com>
2026-09-13 00:06:50 +0530

    Time: 2 ms (93.13%) | Memory: 72 MB (55.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,6 +4,7 @@ class Solution {
         int prod = 1;
         int count = 0;
         int[] product = new int[n];
+
         for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0){
                 count++;
@@ -14,9 +15,11 @@ class Solution {
 
         
         for(int i = 0 ; i < n ; i++){
+            //if no zeros then normal division
             if(count == 0){
                 product[i] = prod / nums[i];
             }
+            //if 1zero then only postion having zero will have prod as an ans
             else if((count == 1)){
                 if(nums[i] == 0){
                     product[i] = prod;
@@ -25,6 +28,7 @@ class Solution {
                     product[i] = 0;
                 }
             }
+            //if more than one zeros , all elements are 0
             else if(count > 1){
                 product[i] = 0;
             }
